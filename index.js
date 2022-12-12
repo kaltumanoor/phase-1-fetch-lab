376d5a1f8d576c276154f89c9d0be18f291d23f5
kaltuma <kaltumanoor370@gmail.com>
2022-12-12 11:02:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+
+
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
 }
 
 function renderBooks(books) {
@@ -9,8 +13,9 @@ function renderBooks(books) {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
-  });
+  }); 
 }
+
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
